bb4d88add84aaef787ce93f0046eacf9104b43c3
Dharmik Sheladiya <57962867+Dharmik1524@users.noreply.github.com>
2025-08-27 23:30:30 +1000

    Error testing done, secure deployment setup

--- src/hooks/useBrandGenerator.js
+++ src/hooks/useBrandGenerator.js
@@ -21,59 +21,6 @@ export default function useBrandGenerator() {
     setError(null);
     setResult(null);
 
-    const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
-
-    const prompt = `You are a branding expert. 
-    Based on the following startup idea: "${idea}", 
-
-    You will analyse this and understand what user actually want and will think on different perspectives based on the keywords in user idea
-
-If the user's input is NOT related to generating a business idea OR does not contain enough information about their business or has some generic text that affect creating a quality brand name, then:
-
-- Generate exactly one short follow-up message to ask the user for more details.
-- Return ONLY valid JSON in this format (no text, no explanation):
-
-[
-  {
-    "name": "Insert your generated follow-up message here."
-  }
-]
-
-- Replace the text inside "name" with your actual generated follow-up message.
-- Do NOT output the words "Insert your generated follow-up message here." or any placeholder.
--Do not include anything except valid JSON.
-
-
-else
-    Generate 3 meaningful brand identity options that is related to user requirement. For example if user entered details related to restaunrant then generate something that is eas to remember and fulfuill all that user asked for.
-   Each options should include:
-  - A unique brand name
-  - A catchy and creative tagline
-  - A short, available domain name, generate domain name that is not generic and most probaby available which is not already registered
-
-  Return ONLY valid JSON in this format (no text, no explanation):
-
-[
-  {
-    "name": "Brand Name 1",
-    "tagline": "Creative Tagline 1",
-    "domain": "brand1.com"
-  },
-  {
-    "name": "Brand Name 2",
-    "tagline": "Creative Tagline 2",
-    "domain": "brand2.com"
-  },
-  {
-    "name": "Brand Name 3",
-    "tagline": "Creative Tagline 3",
-    "domain": "brand3.com"
-  }
-]
-
-
-`;
-
     try {
       const res = await fetch("/api/generate", {
         method: "POST",
